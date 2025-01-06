fd676651a09952f0e06d87d3e95ceda1b638cb53
Kjeld Flarup <kfa@deif.com>
2025-01-06 13:35:29 +0100

    modbus_server unit test leak fix. Disable modbus bug test.
    Signed-off-by: Jens Steen Krogh <jkh@deif.com>

--- src/modbus-tcp-server.c
+++ src/modbus-tcp-server.c
@@ -1,17 +1,17 @@
-/*
- * Copyright © 2016 DEIF A/S
+/**
+ * @file
+ * This implementation is a copy from the PCM5.2 SDK. The source is located
+ * here https://github.com/deif-wpt/libmodbus/tree/add-tcp-server
  *
- * SPDX-License-Identifier: LGPL-2.1+
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <inttypes.h>
 #ifndef _MSC_VER
 # include <unistd.h>
 #endif
-#include <time.h>
 
 #if defined(_WIN32)
 # define OS_WIN32
@@ -34,20 +34,23 @@
 # include <netdb.h>
 #endif
 
-#include "modbus.h"
-#include "modbus-private.h"
 #include "modbus-tcp-server.h"
 #include <pthread.h>
 #include <time.h>
 
-#include <modbus.h>
+/// scanbuild is buggy, sigh
+#define USE_ASSERT 0
+
+#if USE_ASSERT
+#include <assert.h>
+#endif
 
 #define MB_TCP_SRV_IDLE_TIMEOUT 60
 
 /* modbus client data */
 struct modbus_tcp_client_t {
     int socket;
-    time_t last_update;
+    uint64_t cycle;
     struct modbus_tcp_client_t* next;
 };
 
@@ -56,8 +59,9 @@ struct _modbus_tcp_server {
     int server_socket;
     fd_set refset;
     fd_set rdset;
-    int16_t fdmax;
-    uint16_t max_connections;
+    int fdmax;
+    unsigned fdcount;
+    unsigned max_connections;
     uint16_t port;
     uint32_t idle_time_sec;
     uint32_t select_to_sec;
@@ -66,128 +70,141 @@ struct _modbus_tcp_server {
 
     modbus_t* ctx;
     /* Linked list of all current connections */
-    struct modbus_tcp_client_t* conn;
+    /* Used as single chained list as scanbuild is broken */
+    struct modbus_tcp_client_t conn;
+    unsigned conn_count;
+    int debug; // debug flag
+    int error_count;
 };
 
-static void _modbus_tcp_srv_rm_cli(modbus_tcp_server_t* data,
-        struct modbus_tcp_client_t* link) {
-    struct modbus_tcp_client_t* sp = NULL;
-    struct modbus_tcp_client_t* tmp = NULL;
-
-    for (sp = data->conn; sp != NULL; sp = sp->next) {
-        if (sp == link) {
-            close(sp->socket);
-            FD_CLR(sp->socket, &data->refset);
-
-            /* If we are pointing to HEAD, remove head */
-            if (tmp == NULL) {
-                data->conn = sp->next;
-                free(sp);
-                break;
-            } else {
-                struct modbus_tcp_client_t* tmp_next = sp->next;
-
-                /* check if last link */
-                if (tmp_next == NULL) {
-                    tmp->next = NULL;
-                    free(sp);
-                } else {
-                    tmp->next = tmp_next;
-                    free(sp);
-                }
-            }
+#if USE_ASSERT
+static bool _cli_in_list(modbus_tcp_server_t* data, struct modbus_tcp_client_t* cli) {
+    struct modbus_tcp_client_t* sp;
+    for (sp = data->conn.next; sp != &data->conn; sp = sp->next) {
+        if (sp == cli) {
+            return true;
         }
-        tmp = sp;
     }
+    return false;
+}
+#endif
+
+static void _modbus_tcp_srv_update_fdmax(modbus_tcp_server_t* data) {
+    struct modbus_tcp_client_t* sp;
+    int fdmax = data->server_socket;
+    unsigned fdcount = 0;
+    for (sp = data->conn.next; sp != &data->conn; sp = sp->next) {
+        if (sp->socket > fdmax) {
+            fdmax = sp->socket;
+        }
+        fdcount++;
+    }
+    data->fdmax = fdmax;
+    data->fdcount = fdcount;
+}
+
+static void _modbus_tcp_srv_rm_cli(modbus_tcp_server_t* data,
+        struct modbus_tcp_client_t* cli) {
+    int socket_fd = cli->socket;
+    close(cli->socket);
+    FD_CLR(cli->socket, &data->refset);
+
+#if USE_ASSERT
+    struct modbus_tcp_client_t* entry = cli;
+#endif
+
+    struct modbus_tcp_client_t** psp = &data->conn.next;
+    while (*psp != &data->conn) {
+      if (*psp == cli) {
+         *psp = cli->next;
+         free(cli);
+         cli = NULL;
+         data->conn_count--;
+         break;
+      }
+      psp = &(*psp)->next;
+    }
+
+
+#if USE_ASSERT
+    assert(cli == NULL);
+    assert(!_cli_in_list(data, entry));
+#endif    
+    _modbus_tcp_srv_update_fdmax(data);
+   if (data->debug)
+   {
+      fprintf(stderr, "MB TCP server on port %d, fd: %d close fdmax: %d conn_count: %u fdcount: %u\n",
+         data->port, socket_fd, data->fdmax, data->conn_count, data->fdcount);
+   }
 }
 
 static void _modbus_tcp_srv_del_oldest_cli(modbus_tcp_server_t* data) {
     struct modbus_tcp_client_t* cli_to_remove = NULL;
-    time_t oldest_time = 0x7FFFFFFF;
+    uint64_t oldest_cycle = UINT64_MAX;
 
     /* Remove client from list of connected clients*/
-    struct modbus_tcp_client_t* sp = NULL;
-    for (sp = data->conn; sp != NULL; sp = sp->next) {
-        if (sp->last_update < oldest_time) {
+    struct modbus_tcp_client_t* sp;
+    for (sp = data->conn.next; sp != &data->conn; sp = sp->next) {
+        if (sp->cycle < oldest_cycle) {
             cli_to_remove = sp;
-            oldest_time = sp->last_update;
+            oldest_cycle = sp->cycle;
         }
     }
 
     /* If we found one, remove it */
     if (cli_to_remove != NULL) {
+        if (data->debug) {
+           fprintf(stderr,
+                    "MB TCP server on port %d, fd: %d close oldest cycle %llu age %llu\n",
+                       data->port, cli_to_remove->socket, (unsigned long long) (data->conn.cycle - cli_to_remove->cycle), (unsigned long long) cli_to_remove->cycle);
+        }
         _modbus_tcp_srv_rm_cli(data, cli_to_remove);
     }
 }
 
 static void _modbus_tcp_srv_add_cli(modbus_tcp_server_t* data, int socket) {
-    struct modbus_tcp_client_t* sp = NULL;
-    int connection_nbr = 0;
-
-    /* Handle HEAD */
-    if (data->conn == NULL) {
-        data->conn = malloc(sizeof(struct modbus_tcp_client_t));
-        data->conn->last_update = time(NULL);
-        data->conn->socket = socket;
-        data->conn->next = NULL;
-
-        /* add to listener*/
-        FD_SET(socket, &data->refset);
-
-        if (socket > data->fdmax) {
-            /* Keep track of the maximum */
-            data->fdmax = socket;
-        }
-        return;
+    /* Modbus specification: if no available slots, remove oldest client when new connects */
+    if (data->conn_count >= data->max_connections) {
+       _modbus_tcp_srv_del_oldest_cli(data);
+       if (data->conn_count >= data->max_connections) {         
+          fprintf(stderr,
+               "MB TCP server stuck at %u max connections - ** INTERNAL ERROR **\n", 
+                  data->conn_count);
+          data->error_count++;
+          return;
+       }
     }
-    connection_nbr++; /* HEAD link */
+    
+    struct modbus_tcp_client_t* sp = malloc(sizeof(*sp));
+    sp->cycle = data->conn.cycle;
+    sp->socket = socket;
+    sp->next = data->conn.next;
+    data->conn.next = sp;
+    data->conn_count++;
 
-    /* When we have HEAD, add new links */
-    for (sp = data->conn; sp != NULL; sp = sp->next) {
-        connection_nbr++;
-        if ((sp->socket == socket) || sp->next == NULL) {
+    /* add to listener*/
+    FD_SET(socket, &data->refset);
 
-            sp->next = malloc(sizeof(struct modbus_tcp_client_t));
-            sp->next->last_update = time(NULL);
-            sp->next->socket = socket;
-            sp->next->next = NULL;
+    _modbus_tcp_srv_update_fdmax(data);
 
-            /* add to listener*/
-            FD_SET(socket, &data->refset);
-
-            if (socket > data->fdmax) {
-                /** Keep track of the maximum */
-                data->fdmax = socket;
-            }
-            break;
-        }
-
-        /* Modbus specification: if no available slots, remove oldest client when new connects */
-        if (connection_nbr >= data->max_connections) {
-            _modbus_tcp_srv_del_oldest_cli(data);
-        }
+    if (data->debug) {
+      fprintf(stderr, "MB _modbus_tcp_srv_add_cli() fd: %d cycle: %llu fdmax: %d conn_count: %u fdcount: %u\n", sp->socket, (unsigned long long) sp->cycle, data->fdmax, data->conn_count, data->fdcount);
+    }
+    if (data->fdcount != data->conn_count)
+    {
+      fprintf(stderr, "MB _modbus_tcp_srv_add_cli() ERROR conn_count: %u != fdcount: %u\n", data->conn_count, data->fdcount);
     }
 }
 
 static void _modbus_tcp_server_stop(modbus_tcp_server_t* srv_ctx) {
 
-    struct modbus_tcp_client_t* tmp = NULL;
-
     if (srv_ctx != NULL) {
-        struct modbus_tcp_client_t* cli = srv_ctx->conn;
+        while (srv_ctx->conn_count > 0) {
+           _modbus_tcp_srv_rm_cli(srv_ctx, srv_ctx->conn.next);
+        }
 
         /* Close modbus server sockets*/
         close(srv_ctx->server_socket);
-
-        /* Loop though linked list and close + free all client data */
-        while (cli != NULL) {
-            close(cli->socket);
-
-            tmp = cli;
-            cli = cli->next;
-            free(tmp);
-            tmp = NULL;
-        }
 
         /* Release main context */
         if (srv_ctx->ctx != NULL) {
@@ -195,29 +212,35 @@ static void _modbus_tcp_server_stop(modbus_tcp_server_t* srv_ctx) {
             srv_ctx->ctx = NULL;
         }
 
+        bool debug = srv_ctx->debug;
         /* Release server data */
-        if (srv_ctx != NULL) {
-            free(srv_ctx);
-            srv_ctx = NULL;
-        }
+        free(srv_ctx);
+
+        if (debug) {
+            fprintf(stderr, "MB TCP server stopped\n");
+        } 
     }
 }
 
-modbus_tcp_server_t* modbus_tcp_server_start(char* ipaddr, uint16_t port, uint16_t max_connections) {
-    modbus_tcp_server_t* data = malloc(sizeof(modbus_tcp_server_t));
-    memset(data, 0, sizeof(modbus_tcp_server_t));
+modbus_tcp_server_t* modbus_tcp_server_start(const char* ipaddr, uint16_t port, uint16_t max_connections) {
+    modbus_tcp_server_t* data;
 
+    /* Check select can support the request connections */
+    if (max_connections > FD_SETSIZE) {
+        errno = EFBIG;
+        return NULL;
+    }
+
+    data = calloc(1, sizeof(*data));
+    if (data == NULL) {
+        errno = ENOMEM;
+        return NULL;
+    }
     data->max_connections = max_connections;
     data->port = port;
     data->idle_time_sec = MB_TCP_SRV_IDLE_TIMEOUT;
     data->select_to_sec = MB_TCP_SRV_BLOCKING_TIMEOUT;
     data->select_to_usec = 0;
-
-    /* Check select can support the request connections */
-    if (data->max_connections > FD_SETSIZE) {
-        errno = EFBIG;
-        return NULL;
-    }
 
     /* Create new server */
     data->ctx = modbus_new_tcp(ipaddr, port);
@@ -243,8 +266,12 @@ modbus_tcp_server_t* modbus_tcp_server_start(char* ipaddr, uint16_t port, uint16
     /* Add the server socket */
     FD_SET(data->server_socket, &data->refset);
 
+    /* Empty single linked list, anchor 'data->conn' references itself */
+    data->conn.next = &data->conn;
+
     /* Keep track of the max file descriptor */
-    data->fdmax = data->server_socket;
+    _modbus_tcp_srv_update_fdmax(data);
+
     return data;
 }
 
@@ -286,7 +313,7 @@ int modbus_tcp_server_handle(modbus_tcp_server_t* srv_ctx,
     }
 
     /* If the context has been destroyed, bailout */
-    if ((srv_ctx == NULL) || (srv_ctx->ctx == NULL)) {
+    if (srv_ctx->ctx == NULL) {
         errno = EBADF;
         return -1;
     }
@@ -300,11 +327,14 @@ int modbus_tcp_server_handle(modbus_tcp_server_t* srv_ctx,
     if (retval == 0) {
         /* timeout, this is OK */
     } else if (retval == -1) ///< Critical error on select, exit
-            {
-         /* select has set errno */
+    {
+        /* select has set errno */
         modbus_tcp_server_stop(srv_ctx);
         return -1;
     } else {
+        // cycle counter used as time, independent of time progression, time change
+        srv_ctx->conn.cycle++;
+
         /* New connection request */
         if (FD_ISSET(srv_ctx->server_socket, &srv_ctx->rdset)) {
             socklen_t addrlen;
@@ -318,21 +348,25 @@ int modbus_tcp_server_handle(modbus_tcp_server_t* srv_ctx,
                     (struct sockaddr *) &clientaddr, &addrlen);
 
             /* Debug if needed */
-            if (srv_ctx->ctx->debug) {
+            if (srv_ctx->debug && newfd > 0) {
                 char ipstr[INET6_ADDRSTRLEN + 1] = { 0 };
                 int port = 0;
 
                 struct sockaddr_in *s = (struct sockaddr_in *) &clientaddr;
                 port = ntohs(s->sin_port);
+                #ifdef USE_VERY_SLOW_NAME_RESOLUTION
                 getnameinfo((struct sockaddr *) &clientaddr, sizeof(clientaddr),
                         ipstr, sizeof(ipstr), NULL, 0, 0);
+                #else
+                inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof(ipstr));
+                #endif
                 fprintf(stderr,
-                        "MB TCP server on port %d, Incoming connection from %s -> %d\n",
-                        srv_ctx->port, ipstr, port);
+                        "MB TCP server on port %d, fd: %d Incoming connection from %s -> %d\n",
+                        srv_ctx->port, newfd, ipstr, port);
             }
 
             if (newfd == -1) {
-                if (srv_ctx->ctx->debug) {
+                if (srv_ctx->debug) {
                     perror("Server accept() error");
                     fprintf(stderr, " Socket: %d on port: %d\r\n",
                             srv_ctx->server_socket, srv_ctx->port);
@@ -343,29 +377,36 @@ int modbus_tcp_server_handle(modbus_tcp_server_t* srv_ctx,
                 _modbus_tcp_srv_add_cli(srv_ctx, newfd);
             }
         }
+
         /* Data request */
-        else {
-            struct modbus_tcp_client_t* sp = NULL;
+         struct modbus_tcp_client_t* sp = srv_ctx->conn.next;
 
-            for (sp = srv_ctx->conn; sp != NULL; sp = sp->next) {
-                if (FD_ISSET(sp->socket, &srv_ctx->rdset)) {
-                    /* An already connected master has sent a new query */
-                    uint8_t query[MODBUS_TCP_MAX_ADU_LENGTH];
+         while (sp != &srv_ctx->conn) {
+            struct modbus_tcp_client_t* next = sp->next;
+               if (FD_ISSET(sp->socket, &srv_ctx->rdset)) {
+                  /* An already connected master has sent a new query */
+                  uint8_t query[MODBUS_TCP_MAX_ADU_LENGTH];
 
-                    modbus_set_socket(srv_ctx->ctx, sp->socket);
-                    rc = modbus_receive(srv_ctx->ctx, query);
+                  modbus_set_socket(srv_ctx->ctx, sp->socket);
+                  rc = modbus_receive(srv_ctx->ctx, query);
 
-                    if (rc != -1) {
-                        modbus_reply(srv_ctx->ctx, query, rc, mb_map);
-                        sp->last_update = time(NULL);;
-                        /* considering implementing a callback pointer with a registration function
-                           which the user can use for a 'request from IP on port has been handled' event. */
-                    } else {
-                        _modbus_tcp_srv_rm_cli(srv_ctx, sp);
-                    }
-                }
-            }
-        }
+                  if (rc != -1) {
+                     modbus_reply(srv_ctx->ctx, query, rc, mb_map);
+                     sp->cycle = srv_ctx->conn.cycle;
+                     /* considering implementing a callback pointer with a registration function
+                        which the user can use for a 'request from IP on port has been handled' event. */
+                  } else {
+                     // sp will properly point to freed memory after this call.
+                     if (srv_ctx->debug) {
+                        fprintf(stderr,
+                                 "MB TCP server on port %d, fd: %d modbus_receive error(%d,%s)\n", 
+                                    srv_ctx->port, sp->socket, errno, strerror(errno));
+                     }
+                     _modbus_tcp_srv_rm_cli(srv_ctx, sp);
+                  }
+               }
+               sp = next;
+         }
     }
     return 0;
 }
@@ -381,3 +422,27 @@ int modbus_tcp_server_set_select_timeout(modbus_tcp_server_t* srv_ctx, uint32_t 
     return 0;
 }
 
+void modbus_tcp_server_debug(modbus_tcp_server_t* mb_srv_ctx, bool enable)
+{
+   if (mb_srv_ctx) {
+      mb_srv_ctx->debug = enable;
+   }
+}
+
+int modbus_tcp_server_error_count(modbus_tcp_server_t* mb_srv_ctx)
+{
+   int rc = -1;
+   if (mb_srv_ctx) {
+      rc = mb_srv_ctx->error_count;
+   }
+   return rc;
+}
+
+int modbus_tcp_server_client_count(modbus_tcp_server_t* mb_srv_ctx)
+{
+   int rc = -1;
+   if (mb_srv_ctx) {
+      rc = mb_srv_ctx->conn_count;
+   }
+   return rc;
+}
--- src/modbus-tcp-server.h
+++ src/modbus-tcp-server.h
@@ -1,12 +1,13 @@
-/*
- * Copyright © 2016 DEIF A/S
- *
- * SPDX-License-Identifier: LGPL-2.1+
- */
+/* DEIF_LEGAL_HEADER - DO NOT REMOVE THIS LINE! */
+
 
 #ifndef MODBUS_TCP_SERVER_H
 #define MODBUS_TCP_SERVER_H
 #include <stdint.h>
+#include <stdbool.h>
+
+// MODBUS_TCP_SERVER_H: Prevent SDK modbus-tcp-server.h from being parsed by compiler
+#define MODBUS_TCP_SERVER_H
 #include "modbus.h"
 
 #define MB_TCP_SRV_BLOCKING_TIMEOUT 0xFFFFFFFF // Use with modbus_tcp_server_set_select_timeout()
@@ -15,11 +16,20 @@ typedef struct _modbus_tcp_server modbus_tcp_server_t;
 
 MODBUS_BEGIN_DECLS
 
-MODBUS_API modbus_tcp_server_t* modbus_tcp_server_start(char* ipaddr, uint16_t port, uint16_t max_connections);
+#ifndef MODBUS_API
+#define MODBUS_API
+#endif
+
+MODBUS_API modbus_tcp_server_t* modbus_tcp_server_start(const char* ipaddr, uint16_t port, uint16_t max_connections);
 MODBUS_API int modbus_tcp_server_stop(modbus_tcp_server_t* mb_srv_ctx);
 MODBUS_API int modbus_tcp_server_handle(modbus_tcp_server_t* mb_srv_ctx, modbus_mapping_t* mb_map);
 
 MODBUS_API int modbus_tcp_server_set_select_timeout(modbus_tcp_server_t* mb_srv_ctx, uint32_t to_sec, uint32_t to_usec);
+
+#define MODBUS_TCP_HAS_DEBUG
+MODBUS_API void modbus_tcp_server_debug(modbus_tcp_server_t* mb_srv_ctx, bool enable);
+MODBUS_API int modbus_tcp_server_error_count(modbus_tcp_server_t* mb_srv_ctx);
+MODBUS_API int modbus_tcp_server_client_count(modbus_tcp_server_t* mb_srv_ctx);
 
 MODBUS_END_DECLS
 
